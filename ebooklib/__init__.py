46d68fd5c22a4a2c8ac589750c7df2ab41c0fae2
Aleksandar Erkalovic <aerkalov@gmail.com>
2013-05-19 11:50:15 +0200

    Fixed #26 - Add additional item types for audio and video files

--- ebooklib/__init__.py
+++ ebooklib/__init__.py
@@ -2,7 +2,7 @@
 
 VERSION = (0, 1, 0)
 
-# ITEMS
+# LIST OF POSSIBLE ITEMS
 ITEM_UNKNOWN = 0
 ITEM_IMAGE = 1
 ITEM_STYLE = 2
@@ -11,15 +11,15 @@ ITEM_NAVIGATION = 4
 ITEM_VECTOR = 5
 ITEM_FONT = 6
 ITEM_VIDEO = 7
+ITEM_AUDIO = 8
 
-
-
-# Extensions
+# EXTENSION MAPPER
 EXTENSIONS = {ITEM_IMAGE: ['.jpg', '.jpeg', '.gif', '.tiff', '.tif', '.png'],
               ITEM_STYLE: ['.css'],
               ITEM_VECTOR: ['.svg'],
               ITEM_FONT: ['.otf', '.woff'],
               ITEM_SCRIPT: ['.js'],
               ITEM_NAVIGATION: ['.ncx'],
-              ITEM_VIDEO: ['.mov', '.mp4', '.avi']
+              ITEM_VIDEO: ['.mov', '.mp4', '.avi'],
+              ITEM_AUDIO: ['.mp3', '.ogg']
               }
