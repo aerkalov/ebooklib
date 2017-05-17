eb2f0e3d44b1c077a30f18dc0929b63da8e7e57b
Aleksandar Erkalovic <aerkalov@gmail.com>
2017-05-17 17:13:06 +0200

    Fixed #123 - Remove unused test files

---
+++ samples/06_parse/README.md
@@ -0,0 +1,8 @@
+Plugins
+=======
+
+How to parse EPUB fule.
+
+## Start
+
+    python parse.py my-book.epub
--- samples/06_parse/parse.py
+++ samples/06_parse/parse.py
@@ -10,14 +10,8 @@ debug(book.metadata)
 debug(book.spine)
 debug(book.toc)
 
-#for it in book.items:
-#    debug( it.get_type())
-
 for x in  book.get_items_of_type(ebooklib.ITEM_IMAGE):
-    debug( x)
+    debug(x)
 
-from ebooklib.plugins import standard, tidyhtml
-
-opts = {'plugins': [standard.SyntaxPlugin(), tidyhtml.TidyPlugin()]}
-
-epub.write_epub('test.epub', book, opts)
+for x in  book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+    debug(x)
