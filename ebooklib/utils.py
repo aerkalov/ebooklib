17ee79c51ba86284ad0f889e68606fc10873163a
Aleksandar Erkalović <aerkalov@gmail.com>
2018-08-07 18:35:40 +0200

    Fixed #163 - epub:type is not properly extracted

--- ebooklib/utils.py
+++ ebooklib/utils.py
@@ -80,19 +80,41 @@ def create_pagebreak(pageref, label=None, html=True):
     return pageref_elem
 
 
+def get_headers(elem):
+    for n in range(1, 7):
+        headers = elem.xpath('./h{}'.format(n))
+
+        if len(headers) > 0:
+            text = headers[0].text_content().strip()
+            if len(text) > 0:
+                return text
+    return None
+
+
 def get_pages(item):
     body = parse_html_string(item.get_body_content())
     pages = []
 
     for elem in body.iter():
         if 'epub:type' in elem.attrib:
-            pages.append((item.get_name(), elem.get('id'), elem.text or elem.get('id')))
+            if elem.get('id') is not None:
+                _text = None
+                
+                if elem.text is not None and elem.text.strip() != '':
+                    _text = elem.text.strip()
+
+                if _text is None:
+                    _text = elem.get('aria-label')
+
+                if _text is None:
+                    _text = get_headers(elem)
+
+                pages.append((item.get_name(), elem.get('id'), _text or elem.get('id')))
 
     return pages
 
 
 def get_pages_for_items(items):
-
     pages_from_docs = [get_pages(item) for item in items]
 
     return [item for pages in pages_from_docs for item in pages]
